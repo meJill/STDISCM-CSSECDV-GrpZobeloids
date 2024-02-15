cba2e5ab4eb675d1a0d50f728dbea7defd1fafe9
AkimFA <104549356+AkimFA@users.noreply.github.com>
2024-02-15 15:44:55 +0800

    Register button UI update

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,33 +1,37 @@
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import classes from './LoginPage.module.css';
-import Card from '../components/ui/Card';
-import React, {useState} from 'react';
-import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom"; // Import Link from react-router-dom
+import classes from "./LoginPage.module.css";
+import Card from "../components/ui/Card";
+import React, { useState } from "react";
+import axios from "axios";
 
 function GotoSignup() {
-  return (
-    <Link to="/signup-page">Sign Up</Link>
-  );
+  return <Link to="/signup-page">Sign Up</Link>;
 }
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/login', { username, password });
-      setError('')
-      console.log('Login successful:', response.data);
+      const response = await axios.post("http://localhost:5000/login", {
+        username,
+        password,
+      });
+      setError("");
+      console.log("Login successful:", response.data);
       //setError(response.data.test.data.toString())
     } catch (error) {
-      setError('Invalid username or password');
-      console.error('Login failed:', error);
+      setError("Invalid username or password");
+      console.error("Login failed:", error);
     }
   };
-
+  const navigate = useNavigate();
+  const navSignUp = () => {
+    navigate("/signup-page");
+  };
   return (
     <div className={classes.LoginContainer}>
       <h1>LOGIN</h1>
@@ -35,28 +39,32 @@ function LoginPage() {
         <form className={classes.form} onSubmit={handleLogin}>
           <div className={classes.control}>
             <label htmlFor="uname">Username</label>
-            <input 
-              type="text" 
-              id="username" 
-              value={username} 
-              onChange={(e) => setUsername(e.target.value)} 
-              required 
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
             />
           </div>
           <div className={classes.control}>
             <label htmlFor="password">Password</label>
-            <input 
-              type="password" 
-              id="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-              required 
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
             />
           </div>
           {error && <p className={classes.error}>{error}</p>}
           <div className={classes.actions}>
-            <button className={classes.signup}><GotoSignup /></button>
-            <button type="submit" className={classes.login}>Login</button>
+            <button className={classes.signup} onClick={navSignUp}>
+              Register
+            </button>
+            <button type="submit" className={classes.login}>
+              Login
+            </button>
           </div>
         </form>
       </Card>
