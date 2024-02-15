95f847149c24e46eabcf5a7f87d1c01fe40dfc5b
AkimFA <104549356+AkimFA@users.noreply.github.com>
2024-02-15 22:19:33 +0800

    Revert "Session in Route.js (WIP)"
    This reverts commit 56fa1b1d37185a1bc57489611533a8a333bb1ab9.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid'; //for Session Token ID Generation
 const express = require(`express`);
 const app = express();
 const controller = require(`../server.js`);
@@ -8,19 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded( {extended: true})); 
 app.use(fileUpload()); 
 
-const session = require('express-session');
-const uuid = uuidv4();
+app.get(`/login`);
 
-console.log('Random UUID:', uuid);
-
-app.use(
-  session({
-    name: 'session',
-    secret: uuid,
-    expires: new Date(Date.now() + 1 * 60 * 60 * 1000), // 1 hour
-  })
-);
-
-app.get('/login');
 
 module.exports = app;
