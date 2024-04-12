73d36890de55afe02a46d089b3ffdcbfd071a520
AkimFA <104549356+AkimFA@users.noreply.github.com>
2024-04-12 17:41:12 +0800

    Added 2nd database
    Still need to add logic regarding data handling

---
+++ config2.js
@@ -0,0 +1,12 @@
+const { google_site_key } = require("./src/config");
+
+module.exports = {
+    database: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '123pass',
+        database: 'test'
+    },
+    google_site_key: '6Lcpx3MpAAAAAABYyHLYpaKknNPuaHoETc8GuyuU' ,
+    google_private_key: '6Lcpx3MpAAAAAN3yvU-WTxnOSOF0Ap3lZSxmdOe9'
+};
